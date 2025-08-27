5b479b1c673131db66122826b4bb4d6a054e8bdb
tvlisboa <tvlisboa@hotmail.com>
2025-08-27 15:58:20 -0300

    project created

---
+++ src/main/java/org/serratec/dev/dslistperfume/DsListPerfumeApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.dev.dslistperfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListPerfumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsListPerfumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/serratec/dev/dslistperfume/DsListPerfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.dev.dslistperfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListPerfumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
