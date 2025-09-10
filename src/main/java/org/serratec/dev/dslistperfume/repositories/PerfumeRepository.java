a3d9380b5a3348b3b9001fdc15a849095fbc7965
tvlisboa <tvlisboa@hotmail.com>
2025-09-10 18:48:35 -0300

    pequenas correcoes com importacoes sem uso

--- src/main/java/org/serratec/dev/dslistperfume/repositories/PerfumeRepository.java
+++ src/main/java/org/serratec/dev/dslistperfume/repositories/PerfumeRepository.java
@@ -4,7 +4,6 @@ import org.serratec.dev.dslistperfume.entities.Perfume;
 import org.serratec.dev.dslistperfume.projections.PerfumeMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
