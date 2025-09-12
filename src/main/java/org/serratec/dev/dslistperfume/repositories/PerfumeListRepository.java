ccd6fd0f34c8edcc2267cec5ab0fd952716a72c9
tvlisboa <tvlisboa@hotmail.com>
2025-09-11 22:52:54 -0300

    implementando reordenacao de lista dos perfumes

--- src/main/java/org/serratec/dev/dslistperfume/repositories/PerfumeListRepository.java
+++ src/main/java/org/serratec/dev/dslistperfume/repositories/PerfumeListRepository.java
@@ -1,8 +1,16 @@
 package org.serratec.dev.dslistperfume.repositories;
+import org.hibernate.sql.Update;
 import org.serratec.dev.dslistperfume.entities.PerfumeList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PerfumeListRepository extends JpaRepository<PerfumeList,Long> {
+
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition " +
+            "WHERE list_id = :listId AND perfume_id = :perfumeId ")
+    void updateBelonging(Long listId, Long perfumeId, Integer newPosition);
 }
--- src/main/java/org/serratec/dev/dslistperfume/services/PerfumeListService.java
+++ src/main/java/org/serratec/dev/dslistperfume/services/PerfumeListService.java
@@ -1,7 +1,9 @@
 package org.serratec.dev.dslistperfume.services;
 import org.serratec.dev.dslistperfume.DTO.PerfumeListDTO;
 import org.serratec.dev.dslistperfume.entities.PerfumeList;
+import org.serratec.dev.dslistperfume.projections.PerfumeMinProjection;
 import org.serratec.dev.dslistperfume.repositories.PerfumeListRepository;
+import org.serratec.dev.dslistperfume.repositories.PerfumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,8 @@ import java.util.List;
 @Service
 public class PerfumeListService {
 
+    @Autowired
+    private PerfumeRepository perfumeRepository;
 
     @Autowired
     private PerfumeListRepository perfumeListRepository;
@@ -18,5 +22,9 @@ public class PerfumeListService {
     public List<PerfumeListDTO> findAll() {
         List<PerfumeList> result = perfumeListRepository.findAll();
         return result.stream().map(x->new PerfumeListDTO(x)).toList();
+    }
+
+    public void move(Long listId, int sourceIndex, int targetIndex) {
+        List<PerfumeMinProjection> list = perfumeRepository.searchByList(listId);
     }
 }
